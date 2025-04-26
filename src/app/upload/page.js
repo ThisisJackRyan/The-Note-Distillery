4c0ac5923f503c28e09c1824f406666d551ba6ba
CEOFYEAST <benton.diebold@me.com>
2025-04-26 15:56:57 -0500

    Moved reducer function to it's own module

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -4,8 +4,10 @@
  */
 
 "use client";
-import { useReducer } from "react";
 import { useRouter } from "next/router";
+import reducer from "./uploadReducer"
+import { initialState } from "./uploadReducer"
+import { useReducer } from "react";
 import { createPortal } from 'react-dom';
 import { addNewNote } from '@/firebase/firestoreFunctions';
 import ImageUpload from '../components/fileUploader'
@@ -13,83 +15,6 @@ import Modal from '../components/modal'
 import FolderSelector from '../components/folderSelector';
 import FolderCreator from '../components/folderModifier';
 import NewNote from '../components/noteModifier';
-
-const initialState = {
-    extractedContent: '',
-    aiSummary: '',
-    newNoteObj: null,
-    selectedFolder: null,
-    showCreateNote: false,
-    showFolderCreator: false,
-    showFolderSelector: false,
-    previousState: "initial_state"
-}
-
-function reducer(uploadState, action){
-    console.log("Upload Action: " + action.type)
-
-    switch(action.type){
-        case "content_uploaded":
-            return {
-                ...uploadState,
-                extractedContent: action.extractedContent ?? uploadState.extractedContent,
-                aiSummary: action.aiSummary ?? uploadState.aiSummary,
-                showCreateNote: true,
-                showFolderCreator: false,
-                showFolderSelector: false,
-                previousState: "initial_state"
-            }
-        
-        case "note_created":
-            return {
-                ...uploadState,
-                newNoteObj: action.newNoteObj ?? uploadState.newNoteObj,
-                showCreateNote: false,
-                showFolderCreator: false,
-                showFolderSelector: true,
-                previousState: "content_uploaded"
-            }
-
-        /**
-         * The previous state is not stored as a safeguard to prevent re-running of actions with side effects
-         */
-        case "existing_folder_selected":
-            return {
-                ...uploadState,
-                selectedFolder: action.selectedFolder ?? uploadState.selectedFolder,
-                showCreateNote: false,
-                showFolderCreator: false,
-                showFolderSelector: false,
-            }
-
-        case "new_folder_selected":
-            return {
-                ...uploadState,
-                showCreateNote: false,
-                showFolderCreator: true,
-                showFolderSelector: false,
-                previousState: "note_created"
-            }
-        
-        case "upload_cancelled":
-            return {
-                ...uploadState,
-                showCreateNote: false,
-                showFolderCreator: false,
-                showFolderSelector: false,
-                previousState: "initial_state"
-            }
-
-        /**
-         * Should only be used before any actions with side-effects are carried out, such as adding the new note to the db or creating a new folder
-         */
-        case "go_back":
-            return reducer(
-                uploadState,
-                { type: uploadState.previousState }
-            )
-    }
-}
 
 export default function ScannerPage() {
     //const router = useRouter()
---
+++ src/app/upload/uploadReducer.js
@@ -0,0 +1,79 @@
+export const initialState = {
+    extractedContent: '',
+    aiSummary: '',
+    newNoteObj: null,
+    selectedFolder: null,
+    showCreateNote: false,
+    showFolderCreator: false,
+    showFolderSelector: false,
+    previousState: "initial_state"
+}
+
+export default function reducer(uploadState, action){
+    console.log("Upload Action: " + action.type)
+
+    switch(action.type){
+        case "content_uploaded":
+            return {
+                ...uploadState,
+                extractedContent: action.extractedContent ?? uploadState.extractedContent,
+                aiSummary: action.aiSummary ?? uploadState.aiSummary,
+                showCreateNote: true,
+                showFolderCreator: false,
+                showFolderSelector: false,
+                previousState: "initial_state"
+            }
+        
+        case "note_created":
+            return {
+                ...uploadState,
+                newNoteObj: action.newNoteObj ?? uploadState.newNoteObj,
+                showCreateNote: false,
+                showFolderCreator: false,
+                showFolderSelector: true,
+                previousState: "content_uploaded"
+            }
+
+        /**
+         * The previous state is not stored as a safeguard to prevent re-running of actions with side effects
+         */
+        case "existing_folder_selected":
+            return {
+                ...uploadState,
+                selectedFolder: action.selectedFolder ?? uploadState.selectedFolder,
+                showCreateNote: false,
+                showFolderCreator: false,
+                showFolderSelector: false,
+            }
+
+        case "new_folder_selected":
+            return {
+                ...uploadState,
+                showCreateNote: false,
+                showFolderCreator: true,
+                showFolderSelector: false,
+                previousState: "note_created"
+            }
+        
+        case "upload_cancelled":
+            return {
+                ...uploadState,
+                showCreateNote: false,
+                showFolderCreator: false,
+                showFolderSelector: false,
+                previousState: "initial_state"
+            }
+
+        default:
+            return uploadState
+
+        /**
+         * Should only be used before any actions with side-effects are carried out, such as adding the new note to the db or creating a new folder
+         */
+        case "go_back":
+            return reducer(
+                uploadState,
+                { type: uploadState.previousState }
+            )
+    }
+}
