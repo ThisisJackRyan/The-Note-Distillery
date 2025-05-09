43d67a392b41f209e460697ff53c3e496fbdfc71
Jack Ryan <thisisjackryan1@gmail.com>
2025-05-08 21:40:00 -0500

    rebase

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,17 +1,21 @@
-export default function Modal({ children, onClose, onGoBack=null, headerText='' }) {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center">
-            <div className="bg-gray-900 border border-gray-300 rounded shadow-lg">
-
-                <div className="flex border-b border-gray-300 justify-between items-center p-1 bg-gray-900 z-1000">
-                    <button 
-                        onClick={onClose} 
-                        className="bg-none border-none cursor-pointer hover:text-amber-300"
-                    >
-                        ✖
-                    </button>
-                    <h2 className="text-white text-lg font-semibold">{headerText}</h2>
-                </div>
+export default function Modal({
+  children,
+  onClose,
+  onGoBack = null,
+  headerText = "",
+}) {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="bg-gray-900 border border-gray-300 rounded shadow-lg">
+        <div className="flex border-b border-gray-300 justify-between items-center p-1 bg-gray-900 z-1000">
+          <button
+            onClick={onClose}
+            className="bg-none border-none cursor-pointer hover:text-amber-300"
+          >
+            ✖
+          </button>
+          <h2 className="text-white text-lg font-semibold">{headerText}</h2>
+        </div>
 
         <div className="p-2">{children}</div>
 
